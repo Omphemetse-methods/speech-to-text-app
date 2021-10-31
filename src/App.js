b48e31ae00f78591678d2f237aa9b582480e32c5
terminator <omphemetse.methods@gmail.com>
2021-10-31 20:32:24 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import "./App.css";
+import PopUp from "./components/PopUp";
 
-function App() {
+const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(
+  process.env.REACT_APP_SPEECH_RECOGNTION_API_KEY
+);
+SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
+
+const Dictaphone = () => {
+  const [message, setMessage] = useState("");
+  const commands = [
+    {
+      command: "reset",
+      callback: () => resetTranscript(),
+    },
+    {
+      command: "shut up",
+      callback: () => setMessage("I wasn\t talking"),
+    },
+    {
+      command: "Hello",
+      callback: () => setMessage("Hi there"),
+    },
+  ];
+
+  const {
+    transcript,
+    resetTranscript,
+    interimTranscipt,
+    finalTranscript,
+    listening,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition({ commands });
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
+
+  const textRef = useRef();
+  const [isCopied, setIsCopied] = useState(false);
+
+  function handleTextSelection() {
+    navigator.clipboard.writeText(transcript);
+    setIsCopied(true);
+  }
+
+  function handleClose() {
+    setIsCopied(false);
+  }
+
+  const listenContinuously = () => {
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: "en-GB",
+    });
+  };
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="min-w-screen min-h-screen bg-gray-100">
+      <section className="flex flex-col items-center justify-center space-y-2">
+        <p className="text-6xl text-gray-600 font-sans pt-12">
+          Speech Recognition
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Turn speech to text.</p>
+
+        <>
+          <div className="row">
+            <div className="spinner spinner-bounce-top"></div>
+          </div>
+        </>
+      </section>
+
+      <section className="flex justify-center items-center">
+        <section className="w-8/12 p-8 space-y-8">
+          <div className="flex justify-center space-x-4">
+            <button
+              type="button"
+              className="px-10 py-3 border-2 border-gray-100"
+              onClick={resetTranscript}
+            >
+              Reset
+            </button>
+            <button
+              type="button"
+              className="px-10 py-3 border-2 border-gray-100"
+              onClick={listenContinuously}
+            >
+              Listen
+            </button>
+            <button
+              type="button"
+              className="px-10 py-3 border-2 border-gray-100"
+              onClick={SpeechRecognition.stopListening}
+            >
+              Stop
+            </button>
+          </div>
+          <div className="border-t-2 space-y-12 p-8 bg-white shadow">
+            <section className="flex justify-end">
+              <button
+                className="shadow-md border-2 border-purple-400 px-12 py-4"
+                onClick={handleTextSelection}
+              >
+                Copy Text
+              </button>
+            </section>
+            <p ref={textRef}>{transcript}</p>
+
+            <section className="p-8 flex items-center space-x-4">
+              <p>{message}</p>
+            </section>
+          </div>
+        </section>
+      </section>
+
+      <PopUp isCopied={isCopied} handleClose={handleClose}></PopUp>
     </div>
   );
-}
-
-export default App;
+};
+export default Dictaphone;
